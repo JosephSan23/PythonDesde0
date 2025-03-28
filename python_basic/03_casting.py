3e9fa266b9ebd5293881717121f9212d31517b6c
Joseph Olarte <josephsantiago2345@gmail.com>
2025-03-28 13:39:47 -0500

    Casteo de tipos de datos

---
+++ python_basic/03_casting.py
@@ -0,0 +1,15 @@
+###
+# Transformar un tipo de valor a otro
+###
+
+print("Conversion de tipos")
+print(int("100") + 2)
+print("100" + str(2))
+
+## En los booleanos el 0 es el unico numero falso los demas daran positivo
+print(bool(3))
+print(bool(0))
+print(bool(-1))
+
+## Los datos deben ser validos para poder transformarlos
+## Con los numeros decimales redondeara al mas cercano si es 1.50 sera 1, si es 1.51 sera 2.
