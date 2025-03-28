afce9c7abe82cfba0b602f6dfdc4794ed3147a5b
Joseph Olarte <josephsantiago2345@gmail.com>
2025-03-28 13:17:08 -0500

    Algunos tipos de datos

--- python_basic/01_print.py
+++ python_basic/01_print.py
@@ -1,1 +1,6 @@
+# 01 - Imprimir en consola => Print
 print("Hola mundo")
+print('Hola como estas paps tambien se puede con solo una comilla')
+
+# Separar la cadena de texto con un guion
+print("python", "es", "brutal", sep = "-")
---
+++ python_basic/02_types.py
@@ -0,0 +1,20 @@
+###
+# Python tiene varios tipos de datos
+# int, float, str, bool, NoneType, list, touple, dict, range, set...
+###
+
+## int = numeros enteros positivos y negativos
+print(type("int = 1,2,3,4,-5,-6..."))
+
+## float = numeros decimales
+print(type("float = 3.14, 3.12, 2.2..."))
+
+## complex = tienen una parte real y una imaginaria
+print(type(1 + 2j))
+
+## booleano = verdadero y falso
+print(type(1 > 2))
+print(type(1 < 2))
+
+## None = no tiene nada ningun valor asignado
+print(type(None))
