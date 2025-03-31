eac56177075cd6562899292118ea0336844720ed
Joseph Olarte <josephsantiago2345@gmail.com>
2025-03-31 14:59:00 -0500

    Condicional if

---
+++ python_basic/02_flow_control/01_if.py
@@ -0,0 +1,24 @@
+###
+# 01 - Sentencia condicionales
+# Permite ejecutar bloques de codigo solo si se cumplen ciertas condiciones
+###
+
+# print("\n Sentencia simple condicional")
+# edad = 17
+# if edad >= 18:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menor de edad")
+#
+print("\n Sentencia simple condicional")
+nota = 9
+
+if nota >= 9:
+    print("¡Sobresaljente!")
+elif nota >= 7 and nota < 9:
+    print("Notable!")
+elif nota >= 5:
+    print("¡Aprobado!")
+else:
+    print("¡No está calificado!")
+
